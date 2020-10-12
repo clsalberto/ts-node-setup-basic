f0f7e5d1e1185b495042f7cbd7450af9faa94ee0
Carlos Alberto <clsalberto@outlook.com>
2020-10-12 01:21:58 -0300

    feat: add babel and eslint import helpers settings

--- jest.config.js
+++ jest.config.js
@@ -3,16 +3,6 @@ const { pathsToModuleNameMapper } = require('ts-jest/utils')
 const { compilerOptions } = require('./tsconfig.json')
 
 module.exports = {
-  // All imported modules in your tests should be mocked automatically
-  // automock: false,
-
-  // Stop running tests after `n` failures
-  // bail: 0,
-
-  // The directory where Jest should store its cached dependency information
-  // cacheDirectory: "/tmp/jest_rs",
-
-  // Automatically clear mock calls and instances between every test
   clearMocks: true,
 
   // Indicates whether the coverage information should be collected while executing the test
--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,3 @@
 import app from './app'
 
-const port = 3333
-
-app.listen(port, () => {
-  console.log(`App running on http://localhost:${port}`)
-})
+app.listen(3333)
